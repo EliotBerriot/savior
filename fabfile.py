ef70ba78676472f8babe36028e4a2c8b4652c641
eliotberriot <contact@eliotberriot.com>
2013-09-25 10:22:21 +0200

    initial commit

---
+++ fabfile.py
@@ -0,0 +1,57 @@
+ # -*- coding: utf-8 -*-
+
+from autosaver import Autosaver, ParseConfigError
+import logging
+import env_settings
+REMOTE_WORKING_DIR = env_settings.REMOTE_WORKING_DIR
+
+env.hosts = env_settings.HOSTS
+env.user = env_settings.USER
+
+logger = logging.getLogger('autosave')
+logger.setLevel(logging.DEBUG)
+
+def autosave(datasets="all", force_save=False):
+	try:
+		a = Autosaver(datasets, force_save)
+		a.save()
+	except ParseConfigError, e:
+		logger.critical("Could not save datasets {0}".format(datasets))
+		
+def clean(datasets="all", force_save=False): # remove saves that are too old
+	try:
+		a = Autosaver(datasets, force_save)
+		a.clean()
+	except ParseConfigError, e:
+		logger.critical("Could not save datasets {0}".format(datasets))
+
+def purge(datasets="all", force_save=False): # remove all saves
+	try:
+		a = Autosaver(datasets, force_save)
+		a.purge()
+	except ParseConfigError, e:
+		logger.critical("Could not save datasets {0}".format(datasets))
+
+def push(branch='master', remote='origin', runlocal=True):
+    if runlocal:
+        # lance la commande en local
+        local("git push %s %s" % (remote, branch))
+    else:
+        # lance la commande sur les serveurs de la liste eng.hosts
+        run("git push %s %s" % (remote, branch))
+ 
+def pull(branch='master', remote='origin', runlocal=True):
+    if runlocal:
+        local("git pull %s %s" % (remote, branch))
+    else:
+        run("git pull %s %s" % (remote, branch))
+ 
+def sync(branch='master', remote='origin', runlocal=True):
+    pull(branch, remote, runlocal)
+    push(branch, remote, runlocal)
+ 
+def deploy(branch='master', remote='origin'):
+    # execute toutes les commandes dans ce dossier
+    with cd(REMOTE_WORKING_DIR):
+        # excute toutes les commandes avec celle-ci avant
+        pull(branch, remote, False)
