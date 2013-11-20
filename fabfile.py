68785d5c50af4428a208bbdbfe87b55d8e148915
eliotberriot <contact@eliotberriot.com>
2013-11-20 14:16:59 +0100

    Add more log info

--- fabfile.py
+++ fabfile.py
@@ -1,37 +1,12 @@
  # -*- coding: utf-8 -*-
-import sys, os
 
-from main import Savior, ParseConfigError
 from fabric.api import local, run, cd, env, prefix
-import logging
 import env_settings
 REMOTE_WORKING_DIR = env_settings.REMOTE_WORKING_DIR
 
 env.hosts = env_settings.HOSTS
 env.user = env_settings.USER
 
-logger = logging.getLogger('savior')
-logger.setLevel(logging.DEBUG)
-def autosave(datasets="all", force_save=False):
-    try:
-        a = Savior(datasets, force_save)
-        a.save()
-    except ParseConfigError, e:
-        logger.critical("Could not save datasets {0}".format(datasets))
-        
-def clean(datasets="all", force_save=False): # remove saves that are too old
-    try:
-        a = Savior(datasets, force_save)
-        a.clean()
-    except ParseConfigError, e:
-        logger.critical("Could not save datasets {0}".format(datasets))
-
-def purge(datasets="all", force_save=False): # remove all saves
-    try:
-        a = Savior(datasets, force_save)
-        a.purge()
-    except ParseConfigError, e:
-        logger.critical("Could not save datasets {0}".format(datasets))
 
 def push(branch='master', remote='origin', runlocal=True):
     if runlocal:
