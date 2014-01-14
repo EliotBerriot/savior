607cffe9905c8f0c64b7fd455ff8a6c6f4a5f482
Eliot Berriot <contact@eliotberriot.com>
2014-01-14 11:24:14 +0100

    changed fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,33 @@
- # -*- coding: utf-8 -*-
 
+from fabric.operations import prompt, sudo
 from fabric.api import local, run, cd, env, prefix
-import env_settings
-REMOTE_WORKING_DIR = env_settings.REMOTE_WORKING_DIR
+import os 
+REMOTE_WORKING_DIR = """/home/eliotberriot/savior"""
+WORKING_DIR=os.path.dirname(os.path.realpath(__file__))
+HOSTS = {
+    "kimsufi": "176.31.98.11",
+    "vps1": "62.4.11.92",
+    "larcenet": "188.165.214.186:666"
+    }
 
-env.hosts = env_settings.HOSTS
-env.user = env_settings.USER
+env.hosts = [HOSTS["larcenet"]]
+env.user = 'eliotberriot'
+shell = '/bin/bash'
+shell_wrapper="/bin/bash -l -c "
 
 
+    
+def start_dev():
+    local('sass --watch "{0}":"{0}"'.format(WORKING_DIR))
+def switch(host="kimsufi"):
+    """
+        Allow you to switch between hosts
+    """
+    try:
+        env.hosts = [HOSTS[host]]
+        print("Host is now {0}".format(env.hosts))
+    except KeyError:
+        print("Host {0} does not exist in HOSTS setting".format(host))
 def push(branch='master', remote='origin', runlocal=True):
     if runlocal:
         # lance la commande en local
@@ -29,5 +49,10 @@ def sync(branch='master', remote='origin', runlocal=True):
 def deploy(branch='master', remote='origin'):
     # execute toutes les commandes dans ce dossier
     with cd(REMOTE_WORKING_DIR):
-        # excute toutes les commandes avec celle-ci avant
         pull(branch, remote, False)
+        
+def fast_commit(message):
+    local("git add .")
+    local('git commit -m "{0}"'.format(message))
+    sync()
+    deploy()
